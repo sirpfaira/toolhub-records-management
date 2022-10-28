0994050648bdd5342141ef75ebfbe782c2e7af0e
Simba Pfaira <spfaira@yahoo.com>
2022-10-28 19:17:57 +0200

    leaderboard: Complete Leaderboard page
    Completing Leaderboard page and gathering mockup data for page testing

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import DATA from '../assets/mockData.json';
 
 const Leaderboard = () => {
-  return <div>Leaderboard</div>;
+  DATA.sort((a, b) =>
+    a.contributions > b.contributions
+      ? -1
+      : b.contributions > a.contributions
+      ? 1
+      : 0
+  );
+
+  return (
+    <div className='container-fluid mt-3'>
+      <h1>LeaderBoard</h1>
+      <p>
+        These are users that provided the most missing fields in the past 30
+        days. Well done!
+      </p>
+
+      <table className='table table-striped table-hover'>
+        <thead className='table-light'>
+          <tr>
+            <th scope='col'>Position</th>
+            <th scope='col'>Name</th>
+            <th scope='col'>Contributions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {DATA.map((person, index) => (
+            <tr>
+              <th scope='row'>{index + 1}</th>
+              <td>{person.name}</td>
+              <td>{person.contributions}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Leaderboard;
